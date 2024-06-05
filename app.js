4ba044662b791c8dec55256db9a5b0992b06e4ca
AshishDhankhar28 <147251015+AshishDhankhar28@users.noreply.github.com>
2024-06-06 00:59:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ array.forEach((element,i) => {
         makeallnormal();
         index=i;
         element.classList.add("hovers");
-        songs.src=`${index+1}.mp3`;
+        songs.src=`Songs/${index+1}.mp3`;
         songs.currentTime=0;
         songs.play();
         master_button.src="Image/pause.svg";
@@ -90,7 +90,7 @@ document.querySelector(".forwardButton").addEventListener('click',()=>{
         array[index].classList.add("hovers");
         array[index-1].classList.remove("hovers");
     }
-    songs.src=`${index+1}.mp3`;
+    songs.src=`Songs/${index+1}.mp3`;
     songs.currentTime=0;
     songs.play();
     master_button.src="Image/pause.svg";
@@ -109,7 +109,7 @@ document.querySelector(".backButton").addEventListener('click',()=>{
         array[index+1].classList.remove("hovers");
     }
     console.log(index);
-    songs.src=`${index+1}.mp3`;
+    songs.src=`Songs/${index+1}.mp3`;
     songs.currentTime=0;
     songs.play();
     master_button.src="Image/pause.svg";
